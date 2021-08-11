5d628f49df7edbc617ac80d1bf52ff2bf32341d8
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2021-08-11 04:22:44 +0200

    Update api.js

--- Public/js/api.js
+++ Public/js/api.js
@@ -1,10 +1,8 @@
-let api_key = "76e3b81483dbd20d3d2776474795231a";
+let api_key = "your api key";
 
 let img_url = "https://image.tmdb.org/t/p/w500";
-let genres_list_http =
-  "https://api.themoviedb.org/3/genre/movie/list?api_key=76e3b81483dbd20d3d2776474795231a&language=en-US";
-let movie_genres_http =
-  "https://api.themoviedb.org/3/discover/movie?api_key=76e3b81483dbd20d3d2776474795231a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
 
 let original_img_url = "https://image.tmdb.org/t/p/original";
 let movie_detail_http = "https://api.themoviedb.org/3/movie";
